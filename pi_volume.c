23ef639641aab241933ba4c41968ec095bfef33a
Danny van Kooten <dannyvankooten@users.noreply.github.com>
2020-02-13 09:27:15 +0100

    use multi-line line string literal without escape sequences

--- pi_volume.c
+++ pi_volume.c
@@ -1,48 +1,47 @@
 #include <stdio.h>
 
-#define HTML "\
-<!DOCTYPE html>\
-<html lang=\"en\">\
-	<head>\
-		<link rel=\"manifest\" href=\"/manifest.json\">\
-		<meta charset=\"utf-8\">\
-		<title>Woonkamer volume</title>\
-		<meta name=\"viewport\" content=\"width=device-width, initial-scale=1, user-scalable=no\">\
-<style> * { margin: 0; padding: 0; }\
-button { font-size:30vh; margin: 0 auto; display: block; background: #111; color: white; border: 0; }\
-body { font-family: sans-serif; background: #111; padding: 24px; }\
-		</style>\
-	</head>\
-	<body>\
-		<form method=\"POST\">\
-			<button name=\"volume\" value=\"up\">▲</button>\
-		</form>\
-		<form method=\"POST\">\
-			<button name=\"volume\" value=\"down\">▼</button>\
-		</form>\
-		<script>\
-			document.addEventListener('submit', function(evt) {\
-				evt.preventDefault();\
-				window.fetch(window.location.href, {\
-					method: 'POST',\
-					body: `volume=${evt.target.volume.value}`\
-				});\
-			});\
-		</script>\
-	</body>\
-</html>"
+const char * HTML = "" 
+    "<!DOCTYPE html>"
+    "<html lang=\"en\">"
+	"<head>"
+		"<link rel=\"manifest\" href=\"/manifest.json\">"
+		"<meta charset=\"utf-8\">"
+		"<title>Woonkamer volume</title>"
+		"<meta name=\"viewport\" content=\"width=device-width, initial-scale=1, user-scalable=no\">"
+        "<style>"
+            "* { margin: 0; padding: 0; }"
+            "button { font-size:30vh; margin: 0 auto; display: block; background: #111; color: white; border: 0; }"
+            "body { font-family: sans-serif; background: #111; padding: 24px; }"
+        "</style>"
+    "</head>"
+    "<body>"
+        "<form method=\"POST\">"
+            "<button name=\"volume\" value=\"up\">▲</button>"
+        "</form>"
+        "<form method=\"POST\">"
+            "<button name=\"volume\" value=\"down\">▼</button>"
+        "</form>"
+        "<script>"
+            "document.addEventListener('submit', function(evt) {"
+                "evt.preventDefault();"
+                "window.fetch(window.location.href, {"
+                    "method: 'POST',"
+                    "body: `volume=${evt.target.volume.value}`"
+                "});"
+            "});"
+        "</script>"
+    "</body>"
+"</html>";
 
-#define MANIFEST "\
-{\
-  \"name\": \"Woonkamer volume\",\
-  \"display\": \"standalone\",\
-  \"background_color\": \"#111\"\
-}"
+const char * MANIFEST = ""
+"{"
+  "\"name\": \"Woonkamer volume\","
+  "\"display\": \"standalone\","
+  "\"background_color\": \"#111\""
+"}";
 
 #define HTTPSERVER_IMPL
 #include "httpserver.h"
-
-void readbuf(char *dest, http_string_t str);
 
 int http_string_compare(struct http_string_s s, char expected[]) {
     return strncmp(s.buf, expected, strlen(expected)) == 0;
@@ -67,12 +66,12 @@ void handle_request(struct http_request_s *request)
     if (http_string_compare(http_request_target(request), "/manifest.json"))
     {
         http_response_header(response, "Content-Type", "application/json");
-        http_response_body(response, MANIFEST, sizeof(MANIFEST) - 1);
+        http_response_body(response, MANIFEST, strlen(MANIFEST));
     }
     else
     {
         http_response_header(response, "Content-Type", "text/html");
-        http_response_body(response, HTML, sizeof(HTML) - 1);
+        http_response_body(response, HTML, strlen(HTML));
     }
     http_respond(request, response);
 }
@@ -81,16 +80,4 @@ int main()
 {
     struct http_server_s *server = http_server_init(8080, handle_request);
     http_server_listen(server);
-}
-
-void readbuf(char *dest, http_string_t str)
-{
-    dest[0] = '\0';
-
-    int i;
-    for (i = 0; i < str.len; i++)
-    {
-        dest[i] = str.buf[i];
-    }
-    dest[i] = '\0';
 }
